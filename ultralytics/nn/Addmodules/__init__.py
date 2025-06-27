e372cdfbb4a64f1bf9d479d12841f60e00e81c49
Yuxiang Gong <151805040+searingchicken@users.noreply.github.com>
2025-06-27 16:52:58 +0800

    Add files via upload

---
+++ ultralytics/nn/Addmodules/__init__.py
@@ -0,0 +1,1 @@
+from .CPAEnhancer import *
---
+++ ultralytics/nn/extra_modules/block.py
@@ -0,0 +1,318 @@
+import torch
+import torch.nn as nn
+
+from einops import rearrange
+
+__all__ = ['SRFD', 'DRFD', 'PCAFFusion', 'CGAFusion', 'CAFM', 'CAFMFusion']
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+try:
+    from mmcv.cnn import build_activation_layer, build_norm_layer
+    from mmcv.ops.modulated_deform_conv import ModulatedDeformConv2d
+    from mmengine.model import constant_init, normal_init
+except ImportError as e:
+    pass
+
+class DyDCNv2(nn.Module):
+    """ModulatedDeformConv2d with normalization layer used in DyHead.
+    This module cannot be configured with `conv_cfg=dict(type='DCNv2')`
+    because DyHead calculates offset and mask from middle-level feature.
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        stride (int | tuple[int], optional): Stride of the convolution.
+            Default: 1.
+        norm_cfg (dict, optional): Config dict for normalization layer.
+            Default: dict(type='GN', num_groups=16, requires_grad=True).
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride=1,
+                 norm_cfg=dict(type='GN', num_groups=16, requires_grad=True)):
+        super().__init__()
+        self.with_norm = norm_cfg is not None
+        bias = not self.with_norm
+        self.conv = ModulatedDeformConv2d(
+            in_channels, out_channels, 3, stride=stride, padding=1, bias=bias)
+        if self.with_norm:
+            self.norm = build_norm_layer(norm_cfg, out_channels)[1]
+
+    def forward(self, x, offset, mask):
+        """Forward function."""
+        x = self.conv(x.contiguous(), offset, mask)
+        if self.with_norm:
+            x = self.norm(x)
+        return x
+
+
+class Cut(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv_fusion = nn.Conv2d(in_channels * 4, out_channels, kernel_size=1, stride=1)
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x0 = x[:, :, 0::2, 0::2]  # x = [B, C, H/2, W/2]
+        x1 = x[:, :, 1::2, 0::2]
+        x2 = x[:, :, 0::2, 1::2]
+        x3 = x[:, :, 1::2, 1::2]
+        x = torch.cat([x0, x1, x2, x3], dim=1)  # x = [B, 4*C, H/2, W/2]
+        x = self.conv_fusion(x)     # x = [B, out_channels, H/2, W/2]
+        x = self.batch_norm(x)
+        return x
+
+class SRFD(nn.Module):
+    def __init__(self, in_channels=3, out_channels=96):
+        super().__init__()
+        out_c14 = int(out_channels / 4)  # out_channels / 4
+        out_c12 = int(out_channels / 2)  # out_channels / 2
+
+        # 7x7 convolution with stride 1 for feature reinforcement, Channels from 3 to 1/4C.
+        self.conv_init = nn.Conv2d(in_channels, out_c14, kernel_size=7, stride=1, padding=3)
+
+        # original size to 2x downsampling layer
+        self.conv_1 = nn.Conv2d(out_c14, out_c12, kernel_size=3, stride=1, padding=1, groups=out_c14)
+        self.conv_x1 = nn.Conv2d(out_c12, out_c12, kernel_size=3, stride=2, padding=1, groups=out_c12)
+        self.batch_norm_x1 = nn.BatchNorm2d(out_c12)
+        self.cut_c = Cut(out_c14, out_c12)
+        self.fusion1 = nn.Conv2d(out_channels, out_c12, kernel_size=1, stride=1)
+
+        # 2x to 4x downsampling layer
+        self.conv_2 = nn.Conv2d(out_c12, out_channels, kernel_size=3, stride=1, padding=1, groups=out_c12)
+        self.conv_x2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, groups=out_channels)
+        self.batch_norm_x2 = nn.BatchNorm2d(out_channels)
+        self.max_m = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.batch_norm_m = nn.BatchNorm2d(out_channels)
+        self.cut_r = Cut(out_c12, out_channels)
+        self.fusion2 = nn.Conv2d(out_channels * 3, out_channels, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        # 7x7 convolution with stride 1 for feature reinforcement, Channels from 3 to 1/4C.
+        x = self.conv_init(x)  # x = [B, C/4, H, W]
+
+    # original size to 2x downsampling layer
+        c = x                   # c = [B, C/4, H, W]
+        # CutD
+        c = self.cut_c(c)       # c = [B, C, H/2, W/2] --> [B, C/2, H/2, W/2]
+        # ConvD
+        x = self.conv_1(x)      # x = [B, C/4, H, W] --> [B, C/2, H/2, W/2]
+        x = self.conv_x1(x)     # x = [B, C/2, H/2, W/2]
+        x = self.batch_norm_x1(x)
+        # Concat + conv
+        x = torch.cat([x, c], dim=1)    # x = [B, C, H/2, W/2]
+        x = self.fusion1(x)     # x = [B, C, H/2, W/2] --> [B, C/2, H/2, W/2]
+
+    # 2x to 4x downsampling layer
+        r = x                   # r = [B, C/2, H/2, W/2]
+        x = self.conv_2(x)      # x = [B, C/2, H/2, W/2] --> [B, C, H/2, W/2]
+        m = x                   # m = [B, C, H/2, W/2]
+        # ConvD
+        x = self.conv_x2(x)     # x = [B, C, H/4, W/4]
+        x = self.batch_norm_x2(x)
+        # MaxD
+        m = self.max_m(m)       # m = [B, C, H/4, W/4]
+        m = self.batch_norm_m(m)
+        # CutD
+        r = self.cut_r(r)       # r = [B, C, H/4, W/4]
+        # Concat + conv
+        x = torch.cat([x, r, m], dim=1)  # x = [B, C*3, H/4, W/4]
+        x = self.fusion2(x)     # x = [B, C*3, H/4, W/4] --> [B, C, H/4, W/4]
+        return x                # x = [B, C, H/4, W/4]
+
+# Deep feature downsampling
+class DRFD(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.cut_c = Cut(in_channels=in_channels, out_channels=out_channels)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, groups=in_channels)
+        self.conv_x = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, groups=out_channels)
+        self.act_x = nn.GELU()
+        self.batch_norm_x = nn.BatchNorm2d(out_channels)
+        self.batch_norm_m = nn.BatchNorm2d(out_channels)
+        self.max_m = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fusion = nn.Conv2d(3 * out_channels, out_channels, kernel_size=1, stride=1)
+
+    def forward(self, x):       # input: x = [B, C, H, W]
+        c = x                   # c = [B, C, H, W]
+        x = self.conv(x)        # x = [B, C, H, W] --> [B, 2C, H, W]
+        m = x                   # m = [B, 2C, H, W]
+
+        # CutD
+        c = self.cut_c(c)       # c = [B, C, H, W] --> [B, 2C, H/2, W/2]
+
+        # ConvD
+        x = self.conv_x(x)      # x = [B, 2C, H, W] --> [B, 2C, H/2, W/2]
+        x = self.act_x(x)
+        x = self.batch_norm_x(x)
+
+        # MaxD
+        m = self.max_m(m)       # m = [B, 2C, H/2, W/2]
+        m = self.batch_norm_m(m)
+
+        # Concat + conv
+        x = torch.cat([c, x, m], dim=1)  # x = [B, 6C, H/2, W/2]
+        x = self.fusion(x)      # x = [B, 6C, H/2, W/2] --> [B, 2C, H/2, W/2]
+
+        return x                # x = [B, 2C, H/2, W/2]
+
+
+class PCAFFusion(nn.Module):
+    def __init__(self, dim, heads):
+        super(PCAFFusion, self).__init__()
+        self.cfam = CAFM(dim, num_heads=heads)
+        self.pa = PixelAttention_CGA(dim)
+        self.conv = nn.Conv2d(dim, dim, 1, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, data):
+        x, y = data
+        initial = x + y
+        pattn1 = self.cfam(initial)
+        pattn2 = self.sigmoid(self.pa(initial, pattn1))
+        result = initial + pattn2 * x + (1 - pattn2) * y
+        result = self.conv(result)
+        return result
+
+class SpatialAttention_CGA(nn.Module):
+    def __init__(self):
+        super(SpatialAttention_CGA, self).__init__()
+        self.sa = nn.Conv2d(2, 1, 7, padding=3, padding_mode='reflect' ,bias=True)
+
+    def forward(self, x):
+        x_avg = torch.mean(x, dim=1, keepdim=True)
+        x_max, _ = torch.max(x, dim=1, keepdim=True)
+        x2 = torch.concat([x_avg, x_max], dim=1)
+        sattn = self.sa(x2)
+        return sattn
+
+
+class ChannelAttention_CGA(nn.Module):
+    def __init__(self, dim, reduction = 8):
+        super(ChannelAttention_CGA, self).__init__()
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.ca = nn.Sequential(
+            nn.Conv2d(dim, dim // reduction, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(dim // reduction, dim, 1, padding=0, bias=True),
+        )
+
+    def forward(self, x):
+        x_gap = self.gap(x)
+        cattn = self.ca(x_gap)
+        return cattn
+
+class PixelAttention_CGA(nn.Module):
+    def __init__(self, dim):
+        super(PixelAttention_CGA, self).__init__()
+        self.pa2 = nn.Conv2d(2 * dim, dim, 7, padding=3, padding_mode='reflect' ,groups=dim, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, pattn1):
+        B, C, H, W = x.shape
+        x = x.unsqueeze(dim=2) # B, C, 1, H, W
+        pattn1 = pattn1.unsqueeze(dim=2) # B, C, 1, H, W
+        x2 = torch.cat([x, pattn1], dim=2) # B, C, 2, H, W
+        x2 = rearrange(x2, 'b c t h w -> b (c t) h w')
+        pattn2 = self.pa2(x2)
+        pattn2 = self.sigmoid(pattn2)
+        return pattn2
+
+class CGAFusion(nn.Module):
+    def __init__(self, dim, reduction=8):
+        super(CGAFusion, self).__init__()
+        self.sa = SpatialAttention_CGA()
+        self.ca = ChannelAttention_CGA(dim, reduction)
+        self.pa = PixelAttention_CGA(dim)
+        self.conv = nn.Conv2d(dim, dim, 1, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, data):
+        x, y = data
+        initial = x + y
+        cattn = self.ca(initial)
+        sattn = self.sa(initial)
+        pattn1 = sattn + cattn
+        pattn2 = self.sigmoid(self.pa(initial, pattn1))
+        result = initial + pattn2 * x + (1 - pattn2) * y
+        result = self.conv(result)
+        return result
+
+## Convolution and Attention Fusion Module  (CAFM)
+class CAFM(nn.Module):
+    def __init__(self, dim, num_heads=8, bias=False):
+        super(CAFM, self).__init__()
+        self.num_heads = num_heads
+        self.temperature = nn.Parameter(torch.ones(num_heads, 1, 1))
+
+        self.qkv = nn.Conv3d(dim, dim*3, kernel_size=(1,1,1), bias=bias)
+        self.qkv_dwconv = nn.Conv3d(dim*3, dim*3, kernel_size=(3,3,3), stride=1, padding=1, groups=dim*3, bias=bias)
+        self.project_out = nn.Conv3d(dim, dim, kernel_size=(1,1,1), bias=bias)
+        self.fc = nn.Conv3d(3*self.num_heads, 9, kernel_size=(1,1,1), bias=True)
+
+        self.dep_conv = nn.Conv3d(9*dim//self.num_heads, dim, kernel_size=(3,3,3), bias=True, groups=dim//self.num_heads, padding=1)
+
+
+    def forward(self, x):
+        b,c,h,w = x.shape
+        x = x.unsqueeze(2)
+        qkv = self.qkv_dwconv(self.qkv(x))
+        qkv = qkv.squeeze(2)
+        f_conv = qkv.permute(0,2,3,1) 
+        f_all = qkv.reshape(f_conv.shape[0], h*w, 3*self.num_heads, -1).permute(0, 2, 1, 3) 
+        f_all = self.fc(f_all.unsqueeze(2))
+        f_all = f_all.squeeze(2)
+
+        #local conv
+        f_conv = f_all.permute(0, 3, 1, 2).reshape(x.shape[0], 9*x.shape[1]//self.num_heads, h, w)
+        f_conv = f_conv.unsqueeze(2)
+        out_conv = self.dep_conv(f_conv) # B, C, H, W
+        out_conv = out_conv.squeeze(2)
+
+        # global SA
+        q,k,v = qkv.chunk(3, dim=1)   
+        
+        q = rearrange(q, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        k = rearrange(k, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        v = rearrange(v, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+
+        q = torch.nn.functional.normalize(q, dim=-1)
+        k = torch.nn.functional.normalize(k, dim=-1)
+
+        attn = (q @ k.transpose(-2, -1)) * self.temperature
+        attn = attn.softmax(dim=-1)
+
+        out = (attn @ v)
+        
+        out = rearrange(out, 'b head c (h w) -> b (head c) h w', head=self.num_heads, h=h, w=w)
+        out = out.unsqueeze(2)
+        out = self.project_out(out)
+        out = out.squeeze(2)
+        output = out + out_conv
+
+        return output
+
+class CAFMFusion(nn.Module):
+    def __init__(self, dim, heads):
+        super(CAFMFusion, self).__init__()
+        self.cfam = CAFM(dim, num_heads=heads)
+        self.pa = PixelAttention_CGA(dim)
+        self.conv = nn.Conv2d(dim, dim, 1, bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, data):
+        x, y = data
+        initial = x + y
+        pattn1 = self.cfam(initial)
+        pattn2 = self.sigmoid(self.pa(initial, pattn1))
+        result = initial + pattn2 * x + (1 - pattn2) * y
+        result = self.conv(result)
+        return result
